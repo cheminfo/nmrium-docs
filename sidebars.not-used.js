9ec8b53237617f31ba7e3f081d4743eb8975e7c4
Luc Patiny <luc@patiny.com>
2021-04-18 11:08:20 +0200

    chore: initial website

---
+++ sidebars.not-used.js
@@ -0,0 +1,22 @@
+module.exports = {
+  docs: [
+    {
+      type: "category",
+      label: "Introduction",
+      items: [],
+    },
+    {
+      collapsed: true,
+      type: "category",
+      label: "Write documentation",
+      items: [
+        "write-docs/getting-started",
+        "write-docs/create-a-page",
+        "write-docs/create-a-document",
+        "write-docs/create-a-blog-post",
+        "write-docs/markdown-features",
+        "write-docs/thank-you",
+      ],
+    },
+  ],
+};
